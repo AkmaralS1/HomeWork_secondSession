b438214813cb643db1b45b746c162d19c7a01880
Akmaral <axma.mail@gmail.com>
2022-10-08 18:20:22 +0600

    Task 10 исправили

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,7 +1,11 @@
-﻿// Задача 10
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
 Console.Clear();
+Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-while (n >=100 )
-    n /= 10;
-int m = n % 10;
-Console.WriteLine(m);
+n = Math.Abs(n);
+while (n < 100 || n > 999)
+n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine((n % 100) / 10);
