2dbb85f2fe32879911323510e6fb3b5188be03f7
Akmaral <axma.mail@gmail.com>
2022-10-08 17:59:35 +0600

    Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task 15
+
+Console.Clear();
+Console.Write("Введитие цифру: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 5 && n < 8)
+    Console.WriteLine("Это выходной день!");
+if (n >= 1 && n <= 5 )
+    Console.WriteLine("Это будний день!");
+else
+    Console.WriteLine("Ошиблись, попробуйте сначала");
