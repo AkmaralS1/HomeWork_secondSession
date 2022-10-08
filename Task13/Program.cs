095002330171a94f69542bd1dc059555cc79ac14
Akmaral <axma.mail@gmail.com>
2022-10-08 17:35:27 +0600

    Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Task 13
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 0;     
+if (n < 100)
+    {
+        Console.Write("Третьей цифры нет");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+if (n > 1000) 
+    a = n / 10;    
+Console.WriteLine($"Третья цифра: {a % 10}");
